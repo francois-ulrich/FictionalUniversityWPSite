c92c5a9c737f0b2228c0062cc87bef4bd6c6cbab
François Ulrich <fulrich3@gmail.com>
2021-01-23 18:02:06 +0100

    timbered page.php and post infos

--- wp-content/themes/fictionaluniversity/page.php
+++ wp-content/themes/fictionaluniversity/page.php
@@ -4,6 +4,6 @@
 use Timber\Timber;
 
 $context = Timber::context();
-$context['posts'] = Timber::get_posts();
+$context['post'] = Timber::get_post();
 
-Timber::render('./views/pages/index.twig', $context);
+Timber::render('./views/pages/single.twig', $context);
